20742b4ee8dfca5f20ad9f433eacafe0a8e463a5
Jordaniakondzo <jordaniakondzo3438@gmail.com>
2023-05-24 02:28:56 +0300

    Adding a new project

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,11 @@ import javax.swing.*;
 
 public class Main {
     public static void main(String[] args){
-        SwingUtilities.invokeLater(new Runnable() {
-            @Override
-            public void run() {
-                Calculateentry entry = new Calculateentry();
-                entry.setVisible(true);
-            }
+        SwingUtilities.invokeLater(() -> {
+            Calculateentry entry = new Calculateentry();
+            entry.setVisible(true);
+            Registration registration = new Registration();
+                    registration.setVisible(true);
         });
     }
 }
